f9e679ae66382a84c0e9eba2a26bc88e470e19e6
Jonathan Kim <kimgiutae@gmail.com>
2024-03-12 00:01:23 -0300

    fix: improve chart aspect ratio

--- src/components/Chart/ChartSkeleton.tsx
+++ src/components/Chart/ChartSkeleton.tsx
@@ -1,12 +1,12 @@
 import styles from './ChartStyles'
-import { AspectRatio, Skeleton } from '@chakra-ui/react'
+import { AspectRatio, Box, Skeleton } from '@chakra-ui/react'
 import type { FC } from 'react'
 
 const ChartSkeleton: FC = () => {
   return (
     <Skeleton sx={styles.scroller}>
-      <AspectRatio maxW='600px' ratio={16 / 9}>
-        <div />
+      <AspectRatio sx={styles.wrapper} ratio={16 / 8}>
+        <Box width={1178} height={598} />
       </AspectRatio>
     </Skeleton>
   )
