d37de9581e831d282690f147a3885834762a05ad
Jonathan Kim <kimgiutae@gmail.com>
2024-03-12 13:18:02 -0300

    fix: enforce light mode

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,33 +1,43 @@
 import { extendTheme } from '@chakra-ui/react'
+import type { SxStyles } from 'types/SxStyles'
+import type { ThemeConfig } from '@chakra-ui/react'
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      html: {
-        h: {
-          md: '100%'
-        },
-        letterSpacing: 'wide',
-        scrollBehavior: 'smooth'
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false
+}
+
+const styles: { global: SxStyles } = {
+  global: {
+    html: {
+      h: {
+        md: '100%'
       },
-      body: {
-        h: {
-          md: '100%'
-        },
-        pt: {
-          md: '68px',
-          xl: '75px'
-        },
-        pos: 'relative',
-        minW: '320px'
+      letterSpacing: 'wide',
+      scrollBehavior: 'smooth'
+    },
+    body: {
+      h: {
+        md: '100%'
       },
-      '#root': {
-        h: {
-          md: '100%'
-        }
+      pt: {
+        md: '68px',
+        xl: '75px'
+      },
+      pos: 'relative',
+      minW: '320px'
+    },
+    '#root': {
+      h: {
+        md: '100%'
       }
     }
   }
+}
+
+const theme = extendTheme({
+  styles,
+  config
 })
 
 export default theme
